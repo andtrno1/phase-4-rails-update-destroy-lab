228eec9f5672783b991164ab5f70e73434f5605b
andtrno1 <andtrno1@gmail.ccom>
2022-08-28 23:16:12 -0400

    Completed Assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,6 +1,5 @@
 class PlantsController < ApplicationController
 
-  # GET /plants
   def index
     plants = Plant.all
     render json: plants
@@ -16,6 +15,20 @@ class PlantsController < ApplicationController
   def create
     plant = Plant.create(plant_params)
     render json: plant, status: :created
+  end
+
+  # PATCH /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant
+  end
+
+  # DELETE /plants/:id
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    plant.destroy
+    head :no_content
   end
 
   private
